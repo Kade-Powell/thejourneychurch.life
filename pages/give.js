3e529fdf05d571d453708011965742c7a0a97482
Kade Powell <kadepowell@gmail.com>
2021-08-29 21:28:04 -0400

    fixes page not rendering on top, removed html global style?

---
+++ pages/give.js
@@ -0,0 +1,25 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Newspaper } from "react-bootstrap-icons";
+
+export default function Give() {
+  return (
+    <>
+      <>
+        <Row>
+          <Col className="text-center text-light bg-success mt-2">
+            <h1>
+              News and Announcments <Newspaper />
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Need to build this page using on contentful so anyone can add posts
+            here if they want
+          </Col>
+        </Row>
+      </>
+    </>
+  );
+}
