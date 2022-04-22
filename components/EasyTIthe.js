ef6d8baa09c8a200fcf5ad17506a7665cfdedfae
Kade Powell <kadepowell@gmail.com>
2022-04-22 11:59:38 -0400

    working on giving form

--- components/EasyTIthe.js
+++ components/EasyTIthe.js
@@ -4,10 +4,9 @@ import Col from "react-bootstrap/Col";
 export const EasyTIthe = () => {
   return (
     <Col>
-      <Script
-        src="https://forms.ministryforms.net/embed.aspx?formId=0ec43da8-2b21-4484-85c1-df51c398d075"
-        strategy="BeforePageInteractive"
-      />
+      <div>
+        <Script src="https://forms.ministryforms.net/embed.aspx?formId=0ec43da8-2b21-4484-85c1-df51c398d075" />
+      </div>
     </Col>
   );
 };
