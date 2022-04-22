279e2038268a12e29657dad8e51e79df9d9e006c
Kade Powell <kadepowell@gmail.com>
2022-04-22 10:23:38 -0400

    adds giving form

--- pages/give.js
+++ pages/give.js
@@ -1,22 +1,26 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Gift } from "react-bootstrap-icons";
+import Script from "next/script";
 
 export default function Give() {
   return (
     <>
-      <>
-        <Row>
-          <Col className="text-center text-light bg-success mt-2">
-            <h1>
-              How to Give <Gift />
-            </h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col>Coming Soon</Col>
-        </Row>
-      </>
+      <Row>
+        <Col className="text-center text-light bg-success mt-2">
+          <h1>
+            How to Give <Gift />
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Script
+            src="https://forms.ministryforms.net/embed.aspx?formId=612566d4-e253-4641-bfc4-1e370b04e42f"
+            strategy="beforeInteractive"
+          />
+        </Col>
+      </Row>
     </>
   );
 }
