e1cf97f5ccd11b5117f2ec2043940b17ea08b124
Kade Powell <kadepowell@gmail.com>
2022-04-22 10:47:02 -0400

    working on giving form

--- components/EasyTIthe.js
+++ components/EasyTIthe.js
@@ -7,6 +7,7 @@ export const EasyTIthe = () => {
         src="https://forms.ministryforms.net/embed.aspx?formId=612566d4-e253-4641-bfc4-1e370b04e42f"
         strategy="beforeInteractive"
       />
+      <div id="12566d4-e253-4641-bfc4-1e370b04e42f"></div>
     </>
   );
 };
