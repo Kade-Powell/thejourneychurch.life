3f68ec34277972a57e07c19761bdb833e9a162ac
Kade Powell <kadepowell@gmail.com>
2022-04-22 12:10:19 -0400

    riggs on the tithe page

--- components/EasyTIthe.js
+++ components/EasyTIthe.js
@@ -5,7 +5,12 @@ export const EasyTIthe = () => {
   return (
     <Col>
       <div>
-        <Script src="https://forms.ministryforms.net/embed.aspx?formId=0ec43da8-2b21-4484-85c1-df51c398d075" />
+        <iframe
+          id="mb-formbuilder-container"
+          data-uniqueid="6066043689178189"
+          src="https://forms.ministryforms.net/viewForm.aspx?formid=0ec43da8-2b21-4484-85c1-df51c398d075&amp;direct-link=&amp;embed=true&amp;frameid=6066043689178189"
+          style={{ width: "100%", height: "778px", border: "0px" }}
+        ></iframe>
       </div>
     </Col>
   );
