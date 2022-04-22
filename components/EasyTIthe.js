3d8f3ab323c737d29f5a2f5e472f4535b626916d
Kade Powell <kadepowell@gmail.com>
2022-04-22 10:53:44 -0400

    working on giving form

--- components/EasyTIthe.js
+++ components/EasyTIthe.js
@@ -1,13 +1,13 @@
 import Script from "next/script";
+import Head from "next/head";
 
 export const EasyTIthe = () => {
   return (
-    <>
+    <Head>
       <Script
         src="https://forms.ministryforms.net/embed.aspx?formId=612566d4-e253-4641-bfc4-1e370b04e42f"
         strategy="beforeInteractive"
       />
-      <div id="12566d4-e253-4641-bfc4-1e370b04e42f"></div>
-    </>
+    </Head>
   );
 };
